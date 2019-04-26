f3aab91b0962cb83325fc720ef0611e5407e2787
Softwarica College Lab <softwarica@softwarica.com>
2019-04-26 08:30:46 +0545

    external camera is loaded

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -4,41 +4,50 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageview;
+    Button button_image;
 
-    private  void checkPermission(){
+
+    private void checkPermission() {
 
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this, new String[]{
 
-                    Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE
-            },0);
+                    Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 0);
         }
     }
 
 
-    private void loadCamera()
-    {
-        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (intent.resolveActivity(getPackageManager())!=null){
+    private void loadCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
 
-                    startActivityForResult(intent,1);
-    }{
+            startActivityForResult(intent, 1);
+        }
+        {
 
 
-                }
+        }
 
     }
-
 
 
     @Override
@@ -46,12 +55,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageview=findViewById(R.id.imageview);
+        button_image=findViewById(R.id.button_image);
+
+        button_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                loadCamera();
+
+            }
+        });
+
 
         checkPermission();
 
     }
 
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap =(Bitmap) extras.get("data");
+                imageview.setImageBitmap(imageBitmap);
+        }
+    }
 }
 
